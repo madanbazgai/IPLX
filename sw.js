6d0c0dfa8b72303cd210399059fa3a40c41b0c3c
Madan Bajgai <madanbajgai@gmail.com>
2023-04-25 21:24:35 +0545

    added 404.html

--- sw.js
+++ sw.js
@@ -1,8 +1,36 @@
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
 const { StaleWhileRevalidate } = workbox.strategies;
+const { precacheAndRoute } = workbox.precaching;
+
+precacheAndRoute([
+    { url: '/404.html', revision: null },
+]);
 
 workbox.routing.registerRoute(
-    ({ request }) => true,
-    new StaleWhileRevalidate()
+    ({ request }) => {
+        return request.destination === 'style' || request.destination === 'document';
+    },
+    new StaleWhileRevalidate({
+        cacheName: 'my-cache',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            }),
+        ],
+    })
 );
+
+workbox.routing.setCatchHandler(({ event }) => {
+    switch (event.request.destination) {
+        case 'document':
+            return caches.match('/404.html');
+        default:
+            return Response.error();
+    }
+});
